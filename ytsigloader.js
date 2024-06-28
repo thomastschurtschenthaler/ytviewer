3ff4d4f041019c90b42e51bc19d08dfe294bbe8a
Thomas Tschurtschenthaler <thomas.tschurtschenthaler@gmail.com>
2024-06-28 10:59:22 +0200

    sigloader back to original

--- ytsigloader.js
+++ ytsigloader.js
@@ -1,3 +1,4 @@
 module.exports = function(source) {
-    return source.replace("${utils.cutAfterJS(subBody)};${functionName}(ncode);", '${subBody.substring(0, subBody.indexOf("}", subBody.indexOf("join"))+1)};${functionName}(ncode);');
+    return source;
+    //return source.replace("${utils.cutAfterJS(subBody)};${functionName}(ncode);", '${subBody.substring(0, subBody.indexOf("}", subBody.indexOf("join"))+1)};${functionName}(ncode);');
 }
